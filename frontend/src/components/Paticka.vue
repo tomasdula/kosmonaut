06269c6036d4a84e585a362f7f1113d5ed80e02e
tomasdula <52302525+tomasdula@users.noreply.github.com>
2019-06-28 03:11:33 +0200

    Code Kosmonaut
    Code for application electronic management cosmonaut

---
+++ frontend/src/components/Paticka.vue
@@ -0,0 +1,33 @@
+<style>
+    #foot {
+              
+        color: steelblue;
+        background: lightblue;
+        padding-bottom: 0.1em;
+        padding-top: 0.4em;
+        width: 100%;
+        
+        
+    }
+    
+   
+    
+
+</style>
+<template>
+    <div id="foot">
+            <p style="text-align: center;">&COPY; 2019</p>
+            
+    </div>
+</template>
+
+<script>
+
+    export default {
+    data() {
+        return {
+            
+        };
+    },
+    }
+</script>
